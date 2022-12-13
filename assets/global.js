0124c286357fc3a3176a34d38cf474d976e9ee90
hienduchuynh <hienduchuynh@gmail.com>
2022-12-13 11:18:22 +0700

    modal

--- assets/global.js
+++ assets/global.js
@@ -494,6 +494,7 @@ class ModalOpener extends HTMLElement {
     if (!button) return;
     button.addEventListener('click', () => {
       const modal = document.querySelector(this.getAttribute('data-modal'));
+      console.log(button);
       if (modal) modal.show(button);
     });
   }
